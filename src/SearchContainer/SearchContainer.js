9515c060720f74e9ea2bde93dcd4966dd3263eb0
“Maxwell <newton.maxwell@gmail.com>
2022-03-10 12:35:36 +0000

    fixed mobile styling on job type checkboxes in header

--- src/SearchContainer/SearchContainer.js
+++ src/SearchContainer/SearchContainer.js
@@ -31,7 +31,7 @@ const SearchContainer = (props) => {
         <div className={'d-flex flex-column justify-content-start gap-1'}>
             <input type="text" className="form-control" placeholder="job title / keyword / skill / company" aria-label="search bar" aria-describedby="search for jobs here" onChange={ (event) => props.handleSearchOnChange(event)}/>
             <div className={'d-flex justify-content-between'}>
-                <div className={'d-flex gap-3'} id={'type-checkbox-container'}>
+                <div className={'d-flex flex-wrap gap-3'} id={'type-checkbox-container'}>
                     <TypeCheckbox type={'Full time'}/>
                     <TypeCheckbox type={'Part time'}/>
                     <TypeCheckbox type={'Contract'}/>
