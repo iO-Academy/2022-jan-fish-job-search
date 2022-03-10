eb929d28cf9fa5751456ff29868aea4834c975e4
Liam <liamlwrnce@gmail.com>
2022-03-10 16:27:55 +0000

    changes made from pr

--- src/JobDescription/JobDescription.js
+++ src/JobDescription/JobDescription.js
@@ -2,7 +2,7 @@ import './JobDescription.scss'
 
 const JobDescription = (props) => {
     return (
-        <div className={'job-description container'}>
+        <div className={'container'}>
             <h3>Job Description:</h3>
             <p>
                 {props.modalData.job_description}
